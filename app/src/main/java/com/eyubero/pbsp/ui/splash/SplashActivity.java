ebaa416ca6e153f0e3a88d34a750363aff888540
eYube <eduyuberol@gmail.com>
2018-07-30 22:53:06 +0200

    splash implementation

--- app/src/main/java/com/eyubero/pbsp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/eyubero/pbsp/ui/splash/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.eyubero.pbsp.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.eyubero.pbsp.R;
+import com.eyubero.pbsp.ui.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,17 +18,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                goToMain();
             }
-        });
+        },3000);
+
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
