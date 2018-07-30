3589b5f4508d784404da4c8bc8bae8affb85985d
eYube <eduyuberol@gmail.com>
2018-07-30 22:58:05 +0200

    main fragments def

---
+++ app/src/main/java/com/eyubero/pbsp/ui/fragments/FlexibilityFragment.java
@@ -0,0 +1,12 @@
+package com.eyubero.pbsp.ui.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
+ * on 30/07/2018 (ノಠ益ಠ)ノ
+ */
+
+public class FlexibilityFragment extends Fragment{
+
+}
---
+++ app/src/main/java/com/eyubero/pbsp/ui/fragments/FunctionalFragment.java
@@ -0,0 +1,9 @@
+package com.eyubero.pbsp.ui.fragments;
+
+/**
+ * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
+ * on 30/07/2018 (ノಠ益ಠ)ノ
+ */
+
+public class FunctionalFragment {
+}
