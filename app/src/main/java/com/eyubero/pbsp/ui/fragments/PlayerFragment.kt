a767bd10d602f90afbdf715a945a49a38632ebef
eYube <eduyuberol@gmail.com>
2018-07-31 18:40:29 +0200

    player fragment and fragments layout

---
+++ app/src/main/java/com/eyubero/pbsp/ui/fragments/PlayerFragment.kt
@@ -0,0 +1,9 @@
+package com.eyubero.pbsp.ui.fragments
+
+import com.eyubero.pbsp.base.BaseFragment
+
+/**
+ * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
+ * on 31/07/2018 (ノಠ益ಠ)ノ
+ */
+class PlayerFragment : BaseFragment()
